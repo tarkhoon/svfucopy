a7e27d4e063ad342fadd5426681c977024b1c5a5
nikita ulyanov <nikita.ulyanov.2002@inbox.ru>
2021-10-29 00:07:41 +0900

    123

---
+++ js/main.js
@@ -0,0 +1,90 @@
+let hovermenu = document.getElementById('menu1'); //Получение выпадающего меню 1
+    hovermenu.onmouseover = over; //Вызов функции over при наведении
+    hovermenu.onmouseout = hide; //Вызов функции hide при наведении
+var display = document.getElementById('menu1'); //получение элемента menu1
+
+function over(){
+    display.classList.add('hs-sub-menu-opened'); //Добавление класса show к элементу (показывает меню)
+}
+
+function hide(){
+    display.classList.remove('hs-sub-menu-opened');//Удаление класса show к элементу (скрывает меню)
+}
+
+    let hovermenu2 = document.getElementById('menu2'); //Получение выпадающего меню 2
+        hovermenu2.onmouseover = over2; //Вызов функции over при наведении
+        hovermenu2.onmouseout = hide2; //Вызов функции hide при наведении
+    var display2 = document.getElementById('menu2'); //получение элемента menu2
+
+    function over2(){
+        display2.classList.add('hs-sub-menu-opened'); //Добавление класса show к элементу (показывает меню)
+    }
+
+    function hide2(){
+        display2.classList.remove('hs-sub-menu-opened');//Удаление класса show к элементу (скрывает меню)
+    }
+
+let hovermenu3 = document.getElementById('menu3'); //Получение выпадающего меню 3
+    hovermenu3.onmouseover = over3; //Вызов функции over при наведении
+    hovermenu3.onmouseout = hide3; //Вызов функции hide при наведении
+var display3 = document.getElementById('menu3'); //получение элемента menu3
+
+function over3(){
+    display3.classList.add('hs-sub-menu-opened'); //Добавление класса show к элементу (показывает меню)
+}
+
+function hide3(){
+    display3.classList.remove('hs-sub-menu-opened');//Удаление класса show к элементу (скрывает меню)
+}
+
+    let hovermenu4 = document.getElementById('menu4'); //Получение выпадающего меню 2
+        hovermenu4.onmouseover = over4; //Вызов функции over при наведении
+        hovermenu4.onmouseout = hide4; //Вызов функции hide при наведении
+    var display4 = document.getElementById('menu4'); //получение элемента menu2
+
+    function over4(){
+        display4.classList.add('hs-sub-menu-opened'); //Добавление класса show к элементу (показывает меню)
+    }
+
+    function hide4(){
+        display4.classList.remove('hs-sub-menu-opened');//Удаление класса show к элементу (скрывает меню)
+    }
+
+let hovermenu5 = document.getElementById('menu5'); //Получение выпадающего меню 5
+    hovermenu5.onmouseover = over5; //Вызов функции over при наведении
+    hovermenu5.onmouseout = hide5; //Вызов функции hide при наведении
+var display5 = document.getElementById('menu5'); //получение элемента menu5
+
+function over5(){
+    display5.classList.add('hs-sub-menu-opened'); //Добавление класса show к элементу (показывает меню)
+}
+
+function hide5(){
+    display5.classList.remove('hs-sub-menu-opened');//Удаление класса show к элементу (скрывает меню)
+}
+
+    let hovermenu6 = document.getElementById('menu6'); //Получение выпадающего меню 6
+        hovermenu6.onmouseover = over6; //Вызов функции over при наведении
+        hovermenu6.onmouseout = hide6; //Вызов функции hide при наведении
+    var display6 = document.getElementById('menu6'); //получение элемента menu6
+
+    function over6(){
+        display6.classList.add('hs-sub-menu-opened'); //Добавление класса show к элементу (показывает меню)
+    }
+
+    function hide6(){
+        display6.classList.remove('hs-sub-menu-opened');//Удаление класса show к элементу (скрывает меню)
+    }
+
+let hovermenu7 = document.getElementById('menu7'); //Получение выпадающего меню 7
+    hovermenu7.onmouseover = over7; //Вызов функции over при наведении
+    hovermenu7.onmouseout = hide7; //Вызов функции hide при наведении
+var display7 = document.getElementById('menu7'); //получение элемента menu7
+
+function over7(){
+    display7.classList.add('hs-sub-menu-opened'); //Добавление класса show к элементу (показывает меню)
+}
+
+function hide7(){
+    display7.classList.remove('hs-sub-menu-opened');//Удаление класса show к элементу (скрывает меню)
+}
